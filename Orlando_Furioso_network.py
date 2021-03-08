4d8b3216a73696127773b3e30ee7d965cfaf0eaa
Federico Magnani <federico.magnani5@studio.unibo.it>
2021-03-08 17:01:28 +0100

    Added comments and changed some variable names.

--- Orlando_Furioso_network.py
+++ Orlando_Furioso_network.py
@@ -24,9 +24,12 @@ args = parser.parse_args();
 citation_threshold = args.threshold
 
 
+
 ### Import data ###
 
-data = pd.read_csv("Characters_per_chapter.csv", index_col=0)
+data = pd.read_csv("data.csv", index_col=0)
+
+
 
 # Factions (as they are at the beginning)
 
@@ -56,6 +59,9 @@ Muslims = [
     'Sobrin'
     ]
 
+
+# Change of the index
+
 Chants = [
     'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X',
     'XI', 'XII', 'XIII', 'XIV', 'XV', 'XVI', 'XVII', 'XVIII', 'XIX', 'XX',
@@ -69,18 +75,29 @@ data = data.set_index('Chants')
 
 
 
+#
+##
+### Graph ###
+##
+#
+
 G = nx.Graph()
 
-### Layers (nodes)
+
+### Layers (nodes) ###
 
 layers = [Christians, data.index.values, Muslims]
-labels = ['Paladini cristiani', 'Canti', 'Guerrieri mori']
+labels = ['Christians', 'Chants', 'Muslims']
 colors = ['darkblue', 'black', 'darkred']
 
+# Adding nodes in groups. Each group shares the 'layer' variable used by the layout function.
+
 for i in range(3):
-    G.add_nodes_from(layers[i], node_color=colors[i], layer=i)
+    G.add_nodes_from(layers[i], layer=i)
     
 pos = nx.multipartite_layout(G, subset_key='layer', align='vertical')
+
+# Drawing nodes in groups, specifying colors and label (used by the legend).
 
 for i in range(3):
     nx.draw_networkx_nodes(G, pos, nodelist=layers[i],
@@ -88,22 +105,24 @@ for i in range(3):
                            label=labels[i])
 
 
-### Edges
+### Edges ###
+
+# Creating weighted edges
 
 for name in Christians:
     for chant in data.index.values:
-        w = data[name][chant]
-        if (w > citation_threshold):
-            G.add_edge(name, chant, weight=w)
+        occurrences = data[name][chant]
+        if (occurrences > citation_threshold):
+            G.add_edge(name, chant, weight=occurrences)
 
 for name in Muslims:
     for chant in data.index.values:
-        w = data[name][chant]
-        if (w > citation_threshold):
-            G.add_edge(name, chant, weight=w)
+        occurrences = data[name][chant]
+        if (occurrences > citation_threshold):
+            G.add_edge(name, chant, weight=occurrences)
 
 
-weights = [0.05*G[u][v]['weight'] for u,v in G.edges]
+# Creating two class of edges, based on the faction to which the node belongs.
 
 edges_blue = []
 edges_red = []
@@ -114,20 +133,27 @@ for edge in G.edges:
     else:
         edges_red.append(edge)
 
-nx.draw_networkx_edges(G, pos, width=weights, alpha=.9, 
+
+# Drawing edges. Width proportional to the weight. Divided in two groups with different colors.
+
+width = [0.05*G[u][v]['weight'] for u,v in G.edges]
+
+nx.draw_networkx_edges(G, pos, width=width, alpha=.9, 
                        edgelist=edges_red, edge_color='red')            
 
-nx.draw_networkx_edges(G, pos, width=weights, alpha=.9, 
+nx.draw_networkx_edges(G, pos, width=width, alpha=.9, 
                        edgelist=edges_blue, edge_color='blue')            
+
 
 ### Legend and title
 
-plt.legend(scatterpoints = 1)
+plt.legend(scatterpoints = 1, loc=1)
 plt.title("Citation threshold: "+str(citation_threshold))
 plt.show()
 
 
 ### Labels - optional ###
+# Bad visualization but useful for debugging
 
 # labels = {}
 # for i in G.nodes:
